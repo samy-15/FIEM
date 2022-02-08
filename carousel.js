07088740774ae98736b1f795d2dfe3bf444ea73a
samy-15 <84139411+samy-15@users.noreply.github.com>
2022-02-08 21:53:27 +0530

    Add files via upload

---
+++ carousel.js
@@ -0,0 +1,41 @@
+/* Carousal 1*/
+
+$('#carousel-1').carousel({
+		interval: 2000,
+		keyboard: false,
+        wrap: true,
+		pause: hover
+});
+
+/* Carousal 2*/
+$('#carousel-2').carousel({
+		interval: 2000,
+		keyboard: false,
+        wrap:true,
+		pause: hover
+});
+
+/* Carousal 3*/
+$('#carousel-3').carousel({
+    interval: 2000,
+    keyboard: false,
+    wrap:true,
+    pause: hover
+});
+
+/* Carousal 4*/
+$('#carousel-4').carousel({
+    interval: 2000,
+    keyboard: false,
+    wrap:true,
+    pause: hover
+});
+
+/* Carousal 5*/
+$('#carousel-5').carousel({
+    interval: 2000,
+    keyboard: false,
+    wrap:true,
+    pause: hover
+});
+
---
+++ index.js
@@ -0,0 +1,22 @@
+var angle = 0;
+function galleryspin(sign) {
+spinner = document.querySelector("#spinner");
+if (!sign) { angle = angle + 60; } else { angle = angle - 60; }
+spinner.setAttribute("style","-webkit-transform: rotateY("+ angle +"deg); -moz-transform: rotateY("+ angle +"deg); transform: rotateY("+ angle +"deg);");
+}
+
+function myFunction() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
